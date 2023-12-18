4aedfd9e6e11c8e670869f5e2eeed301ba6b8f1c
20110656@student.hcmute.edu.vn <20110656@student.hcmute.edu.vn>
2023-12-19 03:36:22 +0700

    Update code to view revenue details by month

--- OnlineShop/Areas/Admin/Controllers/HomeController.cs
+++ OnlineShop/Areas/Admin/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Models;
+using OnlineShop.ViewModels;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
@@ -59,19 +60,33 @@ namespace OnlineShop.Areas.Admin.Controllers
             ViewBag.totalUsers = totalUsers(year);
             ViewBag.totalOrders = totalOrders(year);
             List<decimal> monthlyRevenueLst = new List<decimal>();
+            List<int> monthlySold = new List<int>();
+            List<int> monthlyOrders = new List<int>();
             for (int i = 1; i <= 12; i++)
             {
                 if (monthlyRevenue(i, year) == 0 && i <= month)
                 {
                     monthlyRevenueLst.Add(0);
+                    monthlySold.Add(0);
+                    monthlyOrders.Add(0);
                 }
                 else if (monthlyRevenue(i, year) == 0 && i > month)
                 {
                     monthlyRevenueLst.Add(0);
+                    monthlySold.Add(0);
+                    monthlyOrders.Add(0);
                 }
                 else
                 {
+                    int numSold = _context.OrderItems.Where(n => n.Order.Date.Value.Month == i
+                                                              && n.Order.Date.Value.Year == year
+                                                              && n.Order.IsPay == 1).Sum(n => n.Count);
+                    int numOrders = _context.Orders.Where(n => n.Date.Value.Month == i
+                                                              && n.Date.Value.Year == year
+                                                              && n.IsPay == 1).Count();
                     monthlyRevenueLst.Add(monthlyRevenue(i, year));
+                    monthlySold.Add(numSold);
+                    monthlyOrders.Add(numOrders);
                 }
             }
             List<int> years = _context.Orders
@@ -81,7 +96,54 @@ namespace OnlineShop.Areas.Admin.Controllers
                             .ToList();
             ViewBag.Years = years;
             ViewBag.monthlyRevenue = monthlyRevenueLst;
+            ViewBag.monthlySold = monthlySold;
+            ViewBag.monthlyOrders = monthlyOrders;
             ViewBag.selectedYear = year;
+            return View();
+        }
+        public IActionResult MonthDetail(int month, int year)
+        {
+            int userId;
+            string roleName = HttpContext.Session.GetString("roleName");
+            bool isNum = int.TryParse(HttpContext.Session.GetString("userId"), out userId);
+            if (!isNum)
+            {
+                return RedirectToAction("SignIn", "Customer", new { area = "Default" });
+            }
+            if (roleName != "Admin")
+            {
+                return RedirectToAction("Index", "Product", new { area = "Default" });
+            }
+            ViewBag.username = _context.Users.Where(n => n.UserId == userId).FirstOrDefault().UserName;
+            ViewBag.Month = month;
+            ViewBag.Year = year;
+            List<Product> products = _context.Products.Where(n => n.IsDeleted == 0
+                                                               && n.IsActive == 1)
+                                                      .Select(n => new Product
+                                                      {
+                                                          ProductId = n.ProductId,
+                                                          ProductName = n.ProductName,
+                                                          PromotionalPrice = n.PromotionalPrice,
+
+                                                      }).Distinct().ToList();
+            List<int> solds = new List<int>();
+            List<decimal> revenues = new List<decimal>();
+            foreach(var product in products)
+            {
+                var query = _context.OrderItems.Where(n => n.ProductId == product.ProductId
+                                                        && n.Order.Date.Value.Month == month
+                                                        && n.Order.Date.Value.Year == year
+                                                        && n.Order.IsPay == 1
+                                                        && n.Order.IsDeleted == 0);
+                int sold = query.Sum(n => n.Count);
+                decimal revenue = (decimal)query.Sum(n => n.Count * n.Product.PromotionalPrice);
+                solds.Add(sold);
+                revenues.Add(revenue);
+            }
+            ViewBag.products = products;
+            ViewBag.revenues = revenues;
+            ViewBag.solds = solds;
+            ViewBag.totalRevenue = revenues.Sum();
             return View();
         }
         public decimal totalRevenue(int year)
