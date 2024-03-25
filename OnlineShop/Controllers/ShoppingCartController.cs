2c7c941c26763d9cfa6ea16585b69413588c3d69
NTL212 <loiloicong121@gmail.com>
2024-03-25 21:54:19 +0700

    Update Payment

--- OnlineShop/Controllers/ShoppingCartController.cs
+++ OnlineShop/Controllers/ShoppingCartController.cs
@@ -235,52 +235,59 @@ namespace OnlineShop.Controllers
 				ViewBag.totalCartItems = cartItems.Sum(n => n.Total);
 				return View(user);
 			}
-			Order order = new Order
-			{
-				UserId = userId,
-				Receiver = receiver,
-				Email = email,
-				Phone = phone,
-				Address = address,
-				StatusId = 1,
-                ShipperId = 1,
-                IsPay = 0,
-				Date = DateTime.Now,
-				IsDeleted = 0
-			};
-			_context.Orders.Add(order);
-			await _context.SaveChangesAsync();
-			int newOrderId = order.OrderId;
-			var lst = _context.CartItems.Where(n => n.Cart.UserId == userId && n.IsDeleted == 0).ToList();
-			foreach(CartItem item in lst)
-            {
-				OrderItem orderItem = new OrderItem
-				{
-					OrderId = newOrderId,
-					ProductId = item.ProductId,
-					Count = item.Count,
-				};
-				_context.OrderItems.Add(orderItem);
-				_context.CartItems.Remove(item);
-				await _context.SaveChangesAsync();
-			}
+			
 
 			if(paymentOption == "4")
 			{
+                Order order = new Order
+                {
+                    UserId = userId,
+                    Receiver = receiver,
+                    Email = email,
+                    Phone = phone,
+                    Address = address,
+                    StatusId = 1,
+                    ShipperId = 1,
+                    IsPay = 0,
+                    Date = DateTime.Now,
+                    IsDeleted = 0
+                };
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+                int newOrderId = order.OrderId;
+                var lst = _context.CartItems.Where(n => n.Cart.UserId == userId && n.IsDeleted == 0).ToList();
+                foreach (CartItem item in lst)
+                {
+                    OrderItem orderItem = new OrderItem
+                    {
+                        OrderId = newOrderId,
+                        ProductId = item.ProductId,
+                        Count = item.Count,
+                    };
+                    _context.OrderItems.Add(orderItem);
+                    _context.CartItems.Remove(item);
+                    await _context.SaveChangesAsync();
+                }
                 return RedirectToAction("Index", "Product");
             }
 			else
 			{
-                var url = URLPayment(int.Parse(paymentOption), newOrderId);
+                HttpContext.Session.SetString("receiverName", receiver);
+                HttpContext.Session.SetString("receiverEmail", email);
+                HttpContext.Session.SetString("receiverPhone", phone);
+                HttpContext.Session.SetString("receiverAddress", address);
+                var url = URLPayment(int.Parse(paymentOption));
                 return Redirect(url);
 			}
 		}
-        public IActionResult VnPayReturn()
+        public async Task<IActionResult> VnPayReturn()
         {
+
             int userId;
             bool isNum = int.TryParse(HttpContext.Session.GetString("userId"), out userId);
             ViewBag.username = _context.Users.Where(n => n.UserId == userId).FirstOrDefault().UserName;
             int cartId = _context.Carts.FirstOrDefault(n => n.UserId == userId).CartId;
+            var lstNew = _context.CartItems.Where(n => n.Cart.UserId == userId && n.IsDeleted == 0).ToList();
             var query = from s1 in _context.Carts.Where(s1 => s1.UserId == userId)
                         join s2 in _context.CartItems on s1.CartId equals s2.CartId
                         select new OrderCartViewModel
@@ -323,20 +330,55 @@ namespace OnlineShop.Controllers
                 long vnp_Amount = Convert.ToInt64(vnpay.GetResponseData("vnp_Amount")) / 100;
                 string bankCode = Request.Query["vnp_BankCode"];
 
+				string receiver = HttpContext.Session.GetString("receiverName");
+
+				string email = HttpContext.Session.GetString("receiverEmail");
+
+
+				string phone = HttpContext.Session.GetString("receiverPhone");
+
+
+				string address = HttpContext.Session.GetString("receiverAddress");
 
                 bool checkSignature = vnpay.ValidateSignature(vnp_SecureHash, vnp_HashSecret);
                 if (checkSignature)
                 {
+                    //Thanh toan thanh cong
                     if (vnp_ResponseCode == "00" && vnp_TransactionStatus == "00")
                     {
                         var order = _context.Orders.FirstOrDefault(x => x.OrderId == int.Parse(orderId));
-                        if (order != null)
+                        if (order == null)
                         {
-                            order.IsPay = 1;//đã thanh toán
-							_context.Update(order);
-                            _context.SaveChanges();
-                        }
-                        //Thanh toan thanh cong
+                            Order orderNew = new Order
+                            {
+                                UserId = userId,
+                                Receiver = receiver,
+                                Email = email,
+                                Phone = phone,
+                                Address = address,
+                                StatusId = 1,
+                                ShipperId = 1,
+                                IsPay = 1,
+                                Date = DateTime.Now,
+                                IsDeleted = 0
+                            };
+                            _context.Orders.Add(orderNew);
+                            await _context.SaveChangesAsync();
+                            int newOrderId = orderNew.OrderId;
+                            
+                            foreach (CartItem item in lstNew)
+                            {
+                                OrderItem orderItem = new OrderItem
+                                {
+                                    OrderId = newOrderId,
+                                    ProductId = item.ProductId,
+                                    Count = item.Count,
+                                };
+                                _context.OrderItems.Add(orderItem);
+                                _context.CartItems.Remove(item);
+                                await _context.SaveChangesAsync();
+                            }
+                        }                      
                         ViewBag.InnerText = "Giao dịch được thực hiện thành công. Cảm ơn quý khách đã sử dụng dịch vụ";
                         //log.InfoFormat("Thanh toan thanh cong, OrderId={0}, VNPAY TranId={1}", orderId, vnpayTranId);
                     }
@@ -357,14 +399,27 @@ namespace OnlineShop.Controllers
             return View();
         }
 
-        public string URLPayment(int TypePaymentVN, int orderId)
+        public string URLPayment(int TypePaymentVN)
 		{
             var urlPayment = "";
-            var order = _context.Orders.FirstOrDefault(x => x.OrderId == orderId);
-            var totalAmount = _context.Orders
-           .Where(o => o.OrderId == order.OrderId)
-           .SelectMany(o => o.OrderItems)
-           .Sum(oi => oi.Count * oi.Product.PromotionalPrice);
+            int userId;
+            bool isNum = int.TryParse(HttpContext.Session.GetString("userId"), out userId);
+            ViewBag.username = _context.Users.Where(n => n.UserId == userId).FirstOrDefault().UserName;
+            int cartId = _context.Carts.FirstOrDefault(n => n.UserId == userId).CartId;
+            var query = from s1 in _context.Carts.Where(s1 => s1.UserId == userId)
+                        join s2 in _context.CartItems on s1.CartId equals s2.CartId
+                        select new OrderCartViewModel
+                        {
+                            CartItemId = s2.CartItemId,
+                            Image = s2.Product.Image,
+                            PromotionalPrice = (decimal)s2.Product.PromotionalPrice,
+                            ProductName = s2.Product.ProductName,
+                            Count = s2.Count,
+                            Total = (decimal)s2.Product.PromotionalPrice * s2.Count
+                        };
+            List<OrderCartViewModel> lst = query.ToList();
+
+            var totalAmount = Convert.ToDouble(lst.Sum(n => n.Total));
             //Get Config Info
             string vnp_Returnurl = _configuration["VnpSettings:ReturnUrl"]; //URL nhan ket qua tra ve 
             string vnp_Url = _configuration["VnpSettings:Url"]; //URL thanh toan cua VNPAY 
@@ -391,16 +446,15 @@ namespace OnlineShop.Controllers
             vnpay.AddRequestData("vnp_CurrCode", "VND");
             vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress(HttpContext));
             vnpay.AddRequestData("vnp_Locale", "vn");
-			var user = _context.Users.FirstOrDefault(u => u.UserId == order.UserId);
+			var user = _context.Users.FirstOrDefault(u => u.UserId == userId);
             vnpay.AddRequestData("vnp_OrderInfo", "Thanh toán đơn hàng của user :" + user.UserName);
             vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
 
             vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
-            vnpay.AddRequestData("vnp_TxnRef", order.OrderId.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
-
-            //Add Params of 2.1.0 Version
-            //Billing
-
+			var orderLastId = _context.Orders.OrderByDescending(x=>x.OrderId).FirstOrDefault().OrderId;
+            vnpay.AddRequestData("vnp_TxnRef", (orderLastId+1).ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
+                                                                            //Add Params of 2.1.0 Version
+                                                                            //Billing
             urlPayment = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
 			//log.InfoFormat("VNPAY URL: {0}", paymentUrl);
 			return urlPayment;
